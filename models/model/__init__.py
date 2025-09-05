d25f52202bd8300a4e7f5e04d5619f2c350cd119
Tan Xinzi <e1129811@u.nus.edu>
2025-09-05 22:46:46 +0800

    Update __init__.py

--- models/model/__init__.py
+++ models/model/__init__.py
@@ -9,6 +9,7 @@ from models.model.torch_model.torch_rmtpp import RMTPP as TorchRMTPP
 from models.model.torch_model.torch_sahp import SAHP as TorchSAHP
 from models.model.torch_model.torch_thp import THP as TorchTHP
 from models.model.torch_model.torch_Hawkesthp import HawkesTHP as TorchHawkesTHP
+from models.model.torch_model.torch_Hawkesthp_shared import HawkesTHP_shared as TorchHawkesTHP_shared
 
 __all__ = ['TorchBaseModel',
            'TorchNHP',
@@ -20,6 +21,7 @@ __all__ = ['TorchBaseModel',
            'TorchODETPP',
            'TorchRMTPP',
            'TorchANHN',
-           'TorchHawkesTHP']
+           'TorchHawkesTHP',
+          'TorchHawkesTHP_shared']
 
 
